760a6fa78623c08d04b327988b620822011c8e67
fis <fis@fiss-mac-3.local>
2016-03-29 12:59:56 +0530

    Initial Commit

---
+++ qwert/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qwert
+//
+//  Created by fis on 3/29/16.
+//  Copyright © 2016 fis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ qwert/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qwert
+//
+//  Created by fis on 3/29/16.
+//  Copyright © 2016 fis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
